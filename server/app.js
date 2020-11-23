e86f455d1d3489c452ba5a95aa21fdee5abd1d34
ykurata <yasukokurata@gmail.com>
2020-11-23 17:20:10 -0500

    set up express server

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up express app
+const app = express();
+
+// Log requests to the console
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// send 404 if no other route matched
+app.use((req, res) => {
+  res.status(404).json({
+    message: 'Route Not Found',
+  });
+});
+
+// setup a global error handler
+app.use((err, req, res, next) => {
+  if (enableGlobalErrorLogging) {
+    console.error(`Global error handler: ${JSON.stringify(err.stack)}`);
+  }
+
+  res.status(500).json({
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
+
+module.exports = app;
