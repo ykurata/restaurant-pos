817754ae68025fa8cbc20e44ae0d2b394885d9e6
ykurata <yasukokurata@gmail.com>
2020-11-23 23:44:41 -0500

    set up sequelize

---
+++ server/config/config.js
@@ -0,0 +1,58 @@
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
+require('dotenv').config();
+
+
+module.exports = {
+
+  // If using onine database
+  // development: {
+  //   use_env_variable: 'DATABASE_URL'
+  // },
+
+  development: {
+    database: 'restaurant-pos',
+    username: 'yasuko',
+    password: null,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+
+  test: {
+    database: 'restaurant-pos',
+    username: 'yasuko',
+    password: null,
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  },
+
+  production: {
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  }
+};
