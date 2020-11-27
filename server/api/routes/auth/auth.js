5ffd93db50e5e76d7d5bb74f27b86b69571e47dc
ykurata <yasukokurata@gmail.com>
2020-11-27 15:53:20 -0500

    tested auth middleware

--- server/api/routes/auth/auth.js
+++ server/api/routes/auth/auth.js
@@ -1,15 +1,16 @@
-const { access } = require('fs');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = function authentication(req, res, next) {
   try {
-    const token = req.header('authorization');
+    const token = req.header('authorization').split(" ")[1];
     if (!token) {
       return res.status(401).json({ error: "Token is no found" });
     }
+    console.log(token);
     jwt.verify(token, process.env.SECRET_KEY, function (err, decode) {
       if (err) {
+        console.log(err);
         return res.status(401).json({ error: "Invalid Token" });
       }
       req.user = decode.id;
